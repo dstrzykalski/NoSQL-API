833c90a2c1c79d11ec84fb02373bedeaa6aa58d2
dstrzykalski <dstrzykalski@yahoo.com>
2024-09-13 20:38:59 -0400

    Finished Index

--- models/index.js
+++ models/index.js
@@ -5,3 +5,23 @@ const thoughtRoutes = require('./routes/thoughtRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Establishes Routes
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+
+// Connects to MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialNetworkDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// fix?
+mongoose.set('debug', true);
+
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+});
